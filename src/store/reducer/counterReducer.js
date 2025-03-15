5c2129f6d9c0c94e8f29fb58890dca061da65b62
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-16 01:11:31 +0700

    innitail

---
+++ src/store/reducer/counterReducer.js
@@ -0,0 +1,15 @@
+import { createStore } from "redux";
+const initial = {
+  counter: 0,
+};
+
+const counterReducer = (state = initial, acton) => {
+  switch (acton.type) {
+    case "increament":
+      return { counter: state.counter + 1 };
+    case "decrement":
+      return { counter: state.counter - 1 };
+    default:
+      return state;
+  }
+};
